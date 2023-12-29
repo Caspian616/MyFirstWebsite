b13c9ab26605877c0b51f77f7d66a3dc564c2d70
Caspian616 <zhengrong616@hotmail.com>
2023-12-30 00:47:29 +0800

    lots of changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 var navLinks = document.getElementById("navLinks");
 var menuBar = document.getElementById("menuBar");
+var title = document.getElementById("title");
 
 function showMenu(){
   navLinks.style.right = "0";
   menuBar.style.visibility = "hidden";
+  title.style.visibility = "hidden";
 }
 
 function hideMenu(){
-  navLinks.style.right = "-200px";
+  navLinks.style.right = "-300px";
   menuBar.style.visibility = "visible";
+  title.style.visibility = "visible";
 }
